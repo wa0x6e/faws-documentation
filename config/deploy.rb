8e63d79fe71ecfcc8830311b9180c0d4439aca7f
Wan Qi Chen <wan@wanki.moe>
2018-07-08 01:37:11 +0400

    Update deployment path

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,5 +2,5 @@ lock '~> 3.11.0'
 
 set :application, 'faws_documentation'
 set :repo_url, 'git@github.com:wa0x6e/faws-documentation.git'
-set :deploy_to, '~/faws_documentation'
+set :deploy_to, '~/developers'
 set :bundle_jobs, 4
