d780eb4dc58e6d497f4a779b3707dc2b5d853282
Wan Qi Chen <wan@wanki.moe>
2019-01-04 15:06:07 +0400

    Deprecate v1 api, and add v2 documentation

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,13 +10,3 @@ server '174.138.118.34',
          forward_agent: true,
          auth_methods: %w[publickey]
        }
-
-namespace :deploy do
-  after :updated, :build_static_files do
-    on roles :web do
-      within "#{release_path}" do
-        execute :bundle, :exec, :middleman, :build, '--clean'
-      end
-    end
-  end
-end
